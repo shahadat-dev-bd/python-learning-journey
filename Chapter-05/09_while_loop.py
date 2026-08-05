4fe2d5b855299b8d288b3d73c64c7bd8c79b8449
shahadat-dev-bd <shahadat.dev.bd@gmail.com>
2026-08-05 09:55:13 -0700

    Add Chapter 5 Lesson 5.2 while loop examples

---
+++ Chapter-05/09_while_loop.py
@@ -0,0 +1,49 @@
+# Example 1
+
+count = 1
+
+while count <= 9:
+    print("Hello")
+    count += 1
+
+
+print("----------------")
+
+
+# Example 2
+
+count = 1
+
+while count <= 5:
+    print("Python")
+    count += 1
+
+print("End")
+
+
+print("----------------")
+
+
+# Example 3
+
+count = 3
+
+while count >= 1:
+    print(count)
+    count -= 1
+
+print("Blast Off!")
+
+
+print("----------------")
+
+
+# Example 4
+
+count = 5
+
+while count >= 1:
+    print("Hello")
+    count -= 2
+
+print("End")
