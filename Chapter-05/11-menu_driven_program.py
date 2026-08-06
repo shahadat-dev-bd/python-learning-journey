df708fdaf8b38ec19fe20ec625a33f7fa7888058
shahadat-dev-bd <shahadat.dev.bd@gmail.com>
2026-08-06 14:02:29 -0700

    Add Chapter 5 menu_driven_program using while loop

---
+++ Chapter-05/11-menu_driven_program.py
@@ -0,0 +1,34 @@
+choice = ""
+
+while choice != "5":
+    print("\n ====Menu====")
+    print("1. Burger")
+    print("2. Pizza")
+    print("3. Coffee")
+    print("4. Pasta")
+    print("5. Exit")
+
+    choice = input("Choose an option (1-5): ")
+
+    if choice == "1":
+        print("Burger Ordered")
+
+    elif choice == "2":
+        print("Pizza Ordered")
+
+    elif choice == "3":
+        print("Coffee Ordered")
+
+    elif choice == "4":
+        print("Pasta Ordered")
+
+    elif choice == "5":
+        print("Thank You!")
+
+    else:
+        print("Invalid Choice. Please select a valid option.")    
+
+
+
+
+          
