45821b440d018ba31bbe3973bcc17679ba96a575
shahadat-dev-bd <shahadat.dev.bd@gmail.com>
2026-08-16 11:27:00 -0700

    Add Chapter 5 loop debugging

---
+++ Chapter-05/20_loop_debugging.py
@@ -0,0 +1,38 @@
+# #wrong code
+# number = 1
+
+# while number <= 5:
+#     print(number)
+#     number -= 1
+
+# #right code
+# while number <= 5:
+#     print(number)
+#     number += 1
+
+# print("===================")
+
+# #wrong code
+# numbers = [10, 20, 30, 40, 50]
+
+# for number in numbers:
+#     if number >= 30:
+#         print(number)
+
+# #right code
+# numbers = [10, 20, 30, 40, 50]
+
+# for number in numbers:
+#     if number <= 30:
+#         print(number)
+
+# print("===================")
+
+students = ["Shahadat", "Rahim", "Karim", "Hasan"]
+marks = [85, 45, 72, 90]
+
+for number, student in enumerate(students, start= 1):
+    if marks[number - 1] >= 50:
+        print(number, ".", student, "→", marks[number - 1], "→ Pass")
+    else:
+        print(number, ".", student, "→", marks[number - 1], "→ Fail")    
