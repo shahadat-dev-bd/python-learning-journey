b482c79d1465b072817d423658b8fc5cd3e701c1
shahadat-dev-bd <shahadat.dev.bd@gmail.com>
2026-08-16 11:28:30 -0700

    Add Chapter 5 loop debugging code update

--- Chapter-05/20_loop_debugging.py
+++ Chapter-05/20_loop_debugging.py
@@ -1,33 +1,45 @@
-# #wrong code
-# number = 1
+#wrong code
+number = 1
 
-# while number <= 5:
-#     print(number)
-#     number -= 1
+while number <= 5:
+    print(number)
+    number -= 1
 
-# #right code
-# while number <= 5:
-#     print(number)
-#     number += 1
+#right code
+while number <= 5:
+    print(number)
+    number += 1
 
-# print("===================")
+print("===================")
 
-# #wrong code
-# numbers = [10, 20, 30, 40, 50]
+#wrong code
+numbers = [10, 20, 30, 40, 50]
 
-# for number in numbers:
-#     if number >= 30:
-#         print(number)
+for number in numbers:
+    if number >= 30:
+        print(number)
 
-# #right code
-# numbers = [10, 20, 30, 40, 50]
+#right code
+numbers = [10, 20, 30, 40, 50]
 
-# for number in numbers:
-#     if number <= 30:
-#         print(number)
+for number in numbers:
+    if number <= 30:
+        print(number)
 
-# print("===================")
+print("===================")
 
+#wrong code
+
+students = ["Shahadat", "Rahim", "Karim", "Hasan"]
+marks = [85, 45, 72, 90]
+
+for number, student in enumerate(students, start= 1):
+    if marks[number] >= 50:
+        print(number, ".", student, "→", marks[number], "→ Pass")
+    else:
+        print(number, ".", student, "→", marks[number], "→ Fail")    
+
+#right code
 students = ["Shahadat", "Rahim", "Karim", "Hasan"]
 marks = [85, 45, 72, 90]
 
